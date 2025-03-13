bc04740cb6bacfdbba30d679e4f153ed61066654
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-03-13 22:33:39 +0530

    commit

--- Scanx_mvc/Controllers/PdfController.cs
+++ Scanx_mvc/Controllers/PdfController.cs
@@ -2,12 +2,13 @@
 using iTextSharp.text.pdf.parser;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using IOPath = System.IO.Path;
 
 public class PdfController : Controller
 {
     public IActionResult Viewer(string fileName)
     {
-        string filePath = Path.Combine("wwwroot/uploads", fileName);
+        string filePath = IOPath.Combine(folderPath, fileName);
         ViewBag.PdfPath = "/uploads/" + fileName;
         ViewBag.PdfText = ExtractTextFromPdf(filePath); // Store extracted text for AI search
         return View();
