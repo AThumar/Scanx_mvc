ae10e1423c510a4219e24e279b2fd1823234491f
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-03-13 21:27:43 +0530

    commit

--- Scanx_mvc/Controllers/HomeController.cs
+++ Scanx_mvc/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Uploadpdf()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
