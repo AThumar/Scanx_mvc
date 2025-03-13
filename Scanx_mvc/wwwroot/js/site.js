785cd4c0584f545e562e9191f0ead5984f7f885c
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-03-13 18:19:24 +0530

    commit

--- Scanx_mvc/wwwroot/js/site.js
+++ Scanx_mvc/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
+﻿
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    let progress = document.querySelector(".progress");
+    let uploaded = 4; // Example
+    let total = 5; // Example
+    progress.style.width = (uploaded / total) * 100 + "%";
+});
